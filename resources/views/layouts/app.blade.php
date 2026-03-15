3e5386ef3fd522327aa8836c3896a45c7fd18c07
Dimsrzqy <dimsrizqyilhami@gmail.com>
2026-03-15 13:13:12 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+    <title>Task App</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="{{url('/task')}}">Home</a></li>
+                <li><a href="{{url('/task/create')}}">Create</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            @yield('content')
+        </div>
+    </main>
+    <footer>
+        <p>&copy; {{date('y')}} Task App</p>
+    </footer>
+</body>
+</html>
